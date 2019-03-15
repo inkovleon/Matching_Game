a08e8d6855480268959e8a57a03cd7229dbe0378
inkovleon <inkovleon@gmail.com>
2019-03-16 00:05:15 +0300

    1

--- main.js
+++ main.js
@@ -51,7 +51,7 @@
                               myGame.gameScreen.setConner();
                               myGame.gameScreen.drawWindow();
 
-                              if(myGame.gScreen==='greeting'){ myGame.gameScreen.showGreeting();}
+                              if(myGame.gScreen==='greeting'){ this.showGreeting();}
                               else if (myGame.gScreen==='game'){
                                             this.showScore();
                                             showCards();
